1fa84de380405ff314391dfdd4e20c913cf71a1a
William R Poulson <jessica.spildener@gmail.com>
2017-08-13 16:16:24 -0400

    questions added with working radio buttons

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,29 @@
 $(document).ready(function() {
-    
+
     function answerCheck() {
         var amountCorrect = 0;
         var amountIncorrect = 0;
         var amountUnanswered = 0;
-        for (var i = 1; i <= 2; i++) {
-            var radios = $("input[name=answer"+i+"]");
+        for (var i = 1; i <= 6; i++) {
+            var radios = $("input[name=answer" + i + "]");
             var wasAnswered = false;
+
             for (var j = 0; j < radios.length; j++) {
                 var radio = radios[j];
                 if (radio.value == "correct" && radio.checked) {
                     amountCorrect++;
                     wasAnswered = true;
-                }else if (radio.value == "wrong" && radio.checked){
+                    $("#correctScore").text(amountCorrect);
+
+                } else if (radio.value == "wrong" && radio.checked) {
                     amountIncorrect++;
                     wasAnswered = true;
+                    $("#incorrectScore").text(amountIncorrect);
                 }
             }
-            if (!wasAnswered){
+            if (!wasAnswered) {
                 amountUnanswered++;
+                $("#unansweredQuestions").text(amountUnanswered);
             }
         }
         console.log("Correct Responses: " + amountCorrect);
@@ -27,6 +32,12 @@ $(document).ready(function() {
     }
 
     $("#submit").on("click", answerCheck);
+
+$("#startButton").on("click", function() {
+    
+
+
+
 
 
 
